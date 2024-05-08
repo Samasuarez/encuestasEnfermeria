4f3dcfd0e6b174f11c9ecce87c602d813afdec42
Samasuarez <paknais_01@hotmail.com>
2024-05-08 16:09:04 -0300

    implementación de renderizado de preguntas y respuestas en el componente Questionnaire

---
+++ frontproyect/src/utils/preguntas..js
@@ -0,0 +1,103 @@
+const preguntas = [
+  {
+    id: 1,
+    pregunta: "¿Cuál es la función principal de una enfermera en un hospital?",
+    respuestas: [
+      "Brindar atención médica directa a los pacientes",
+      "Administrar el hospital y su personal",
+      "Realizar cirugías y procedimientos médicos",
+    ],
+  },
+  {
+    id: 2,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor la función de un enfermero de cuidados intensivos?",
+    respuestas: [
+      "Monitorear y cuidar a pacientes con condiciones médicas críticas",
+      "Realizar exámenes médicos de rutina en pacientes ambulatorios",
+      "Brindar terapia ocupacional a pacientes con discapacidades físicas",
+    ],
+  },
+  {
+    id: 3,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor el papel de una enfermera en la prevención de infecciones?",
+    respuestas: [
+      "Educar a los pacientes sobre medidas de higiene y prevención de infecciones",
+      "Administrar vacunas a pacientes de todas las edades",
+      "Realizar cirugías y procedimientos médicos",
+    ],
+  },
+  {
+    id: 4,
+    pregunta:
+      "¿Qué función tiene una enfermera en el cuidado de pacientes pediátricos?",
+    respuestas: [
+      "Administrar tratamientos médicos específicos según la edad del paciente",
+      "Brindar apoyo emocional tanto a los pacientes como a sus familias",
+      "Realizar procedimientos quirúrgicos complejos en niños",
+    ],
+  },
+  {
+    id: 5,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor el papel de una enfermera en la unidad de cuidados intensivos neonatales?",
+    respuestas: [
+      "Monitorear y cuidar a recién nacidos prematuros o con problemas de salud",
+      "Administrar tratamientos médicos específicos según la edad del paciente",
+      "Brindar terapia ocupacional a pacientes con discapacidades físicas",
+    ],
+  },
+  {
+    id: 6,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor el papel de una enfermera en la unidad de cuidados intensivos para adultos?",
+    respuestas: [
+      "Monitorear y cuidar a pacientes adultos con condiciones médicas críticas",
+      "Administrar vacunas a pacientes de todas las edades",
+      "Brindar terapia ocupacional a pacientes con discapacidades físicas",
+    ],
+  },
+  {
+    id: 7,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor el papel de una enfermera en un centro de atención primaria?",
+    respuestas: [
+      "Realizar exámenes médicos de rutina en pacientes ambulatorios",
+      "Administrar tratamientos médicos específicos según la edad del paciente",
+      "Brindar atención médica directa a los pacientes",
+    ],
+  },
+  {
+    id: 8,
+    pregunta:
+      "¿Qué habilidad es fundamental para una enfermera que trabaja en un servicio de emergencias?",
+    respuestas: [
+      "Realizar evaluaciones rápidas y tomar decisiones bajo presión",
+      "Administrar tratamientos médicos específicos según la edad del paciente",
+      "Brindar apoyo emocional tanto a los pacientes como a sus familias",
+    ],
+  },
+  {
+    id: 9,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor la función de una enfermera en un centro de rehabilitación?",
+    respuestas: [
+      "Brindar terapia física y emocional a pacientes en proceso de recuperación",
+      "Administrar tratamientos médicos específicos según la edad del paciente",
+      "Realizar evaluaciones rápidas y tomar decisiones bajo presión",
+    ],
+  },
+  {
+    id: 10,
+    pregunta:
+      "¿Cuál de las siguientes opciones describe mejor el papel de una enfermera en un centro de atención a largo plazo?",
+    respuestas: [
+      "Brindar cuidados a largo plazo a pacientes con enfermedades crónicas o discapacidades",
+      "Administrar vacunas a pacientes de todas las edades",
+      "Realizar exámenes médicos de rutina en pacientes ambulatorios",
+    ],
+  },
+];
+
+export default preguntas;
