09cf7370717c92a540e146a0486fc2308d1bd238
Samasuarez <paknais_01@hotmail.com>
2024-05-09 18:49:04 -0300

    cambios en el slice

--- frontproyect/src/redux/questionnaireSlice.js
+++ frontproyect/src/redux/questionnaireSlice.js
@@ -1,25 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const enviarCuestionario = async () => {
-  try {
-    const respuestas = obtenerRespuestasDelFormulario(); 
-
-    const response = await fetch('http://localhost:4000/api/cuestionario', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ respuestas }),
-    });
-
-    const data = await response.json();
-    console.log(data); 
-  } catch (error) {
-    console.error('Error al enviar el cuestionario:', error);
-  }
-};
-
-
 const questionnaireSlice = createSlice({
   name: "questionnaire",
   initialState: {
